3b986c7e2b843c4f0a2f9984ce0ae00b1756c7d3
Arthur <arthurthehead@gmail.com>
2020-06-13 16:54:56 +0600

    update second task/part1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,65 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        
+
+
 
         static void Main(string[] args)
         {
+            string[] values =
+                             {
+                                "GeoMeta",
+                                "Государственная система обеспечения градостроительной деятельности",
+                                "Городская аналитика",
+                                "Другие наши проекты "
+                            };
+
             Test test = new Test();
-            test.test2();
-            test.Test1();
+            ArrayList valuesH1H2 = new ArrayList();
+            ArrayList valuesH1H2Result = new ArrayList();
+
+            valuesH1H2Result.AddRange(test.Test3());
+            valuesH1H2.AddRange(values);
+            foreach (string elem in valuesH1H2Result)
+            {
+                Console.WriteLine($"result value ->> {elem}");
+            }
+
+
+            //test.test2();
+            //test.Test1();
+            //test.Test3();
         }
 
-      
+
     }
 
-    class Test 
+    class Test
     {
         IWebDriver driver = new ChromeDriver("C:\\Users\\SUN\\Downloads\\chromedriver_win32");
         IWebElement webElement;
         private string urlGems = "https://gemsdev.ru";
-        public void Test1()
+
+
+
+        //public void Test1()
+        //{
+        //    valuesH1H2.AddRange(values);
+
+        //    driver.Manage().Window.Maximize();
+        //    driver.Navigate().GoToUrl(urlGems);
+        //    webElement = driver.FindElement(By.LinkText("Продукты"));
+
+        //    webElement.Click();
+
+        //    //int ScrollHeight = 0;
+        //    IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+        //    for(int i = 0; i<4; i++) 
+        //    { 
+        //            var newScrollHeight = js.ExecuteScript(
+        //                "window.scrollBy(0,600)"
+        //                );
+        //            Thread.Sleep(1000);
+        //    }
+        //    List<IWebElement> webH2 = driver.FindElements(By.CssSelector("h1, h2")).ToList();
+
+        //    ArrayList valuesH1H2New = new ArrayList();
+        //    for (int i = 0; i < webH2.Count; i++)
+        //    {
+        //        // string res = webH2[i].Text;
+        //        valuesH1H2New.Add(webH2[i].Text);
+        //        Console.WriteLine($"\nh2 {i} -> {webH2[i].Text}");
+
+        //    }
+
+        //    //valuesH1H2.Contains(webH2);
+        //    for (int i=0; i < webH2.Count; i++)
+        //    {
+
+        //        Console.WriteLine($"in ArrayList -> {valuesH1H2[i]} - {valuesH1H2New[i]} <- in resArray");
+        //        Console.ReadLine();
+        //    }
+        //    // string res = driver.FindElement(By.CssSelector("h1, h2")).Text;
+        //    // Console.WriteLine($"text in h is -> {res}");
+        //    driver.Quit();
+
+        //}
+
+        //public void test2()
+        //{
+        //    string findUrl = "https://refactoring.guru/ru/design-patterns/csharp";
+        //    driver.Manage().Window.Maximize();
+        //    driver.Navigate().GoToUrl(findUrl);
+
+        //    string res = driver.FindElement(By.CssSelector("body > div.body-holder > main > div > div > div.dp-text > h2")).Text;
+        //    Console.WriteLine($"text in h is -> {res}");
+        //    driver.Quit();
+        //    //driver.FindElement(By.XPath());
+
+        //}
+
+
+        public ArrayList Test3()
         {
+
+            string[] res = { };
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(urlGems);
             webElement = driver.FindElement(By.LinkText("Продукты"));
 
             webElement.Click();
 
-            //List <IWebElement> h2elements = driver.FindElement(By.CssSelector(".container h2")).ToList();
-            // List <IWebElement> webH2 = driver.FindElements(By.CssSelector("body > section.my-150 > div > div > div.col-12.col-md-6.fadeInDown > h1")).ToList();
-            //// List<IWebElement> webH3 = driver.FindElements(By.CssSelector(".col-12 p ")).ToList();
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            for (int i = 0; i < 4; i++)
+            {
+                var newScrollHeight = js.ExecuteScript(
+                    "window.scrollBy(0,500)"
+                    );
+                Thread.Sleep(1000);
+            }
 
-            //for (int i = 0; i < webH2.Count; i++)
-            //{
-            //   // string res = webH2[i].Text;
-            //    Console.WriteLine($"\nh2 {i} -> {webH2[i].Text}");
+            List<IWebElement> webH2 = driver.FindElements(By.CssSelector("h1,h2")).ToList();
+            ArrayList valuesH1H2New = new ArrayList();
 
-            //}
-
-            //webElement = driver.FindElement(By.XPath("/html/body/section[2]/div[2]/div[1]/div[2]/h2/text()"));
-            
-            
-            string res = driver.FindElement(By.CssSelector("body > section.my-150 > div > div > div.col-12.col-md-6.fadeInDown > h1")).Text;
-            Console.WriteLine($"h2 is -> {res}");
+            for (int i = 0; i < webH2.Count; i++)
+            {
+                valuesH1H2New.Add(webH2[i].Text);
+            }
+            foreach (string elem in valuesH1H2New)
+            {
+                Console.WriteLine($"In test function -> {elem}");
+            }
             driver.Quit();
+            return valuesH1H2New;
 
-        }
 
-        public void test2()
-        {
-            string findUrl = "https://xn--c1aaceme9acfqh.xn--p1ai/";
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl(urlGems);
-            driver.FindElement(By.XPath());
-            
         }
     }
 
--- gemsdev.ru.Test/UnitTest1.cs
+++ gemsdev.ru.Test/UnitTest1.cs
@@ -4,6 +4,8 @@ using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Linq;
+using System.Collections;
+using System.Threading;
 using System.Net.Http.Headers;
 
 namespace gemsdev.ru.Test
@@ -13,50 +15,66 @@ namespace gemsdev.ru.Test
         private IWebDriver driver = new ChromeDriver("C:\\Users\\SUN\\Downloads\\chromedriver_win32");
         private IWebElement webElement;
 
-            
+
         private string urlGems = "https://gemsdev.ru";
         private string[] values =
         {
-                                "Государственная система обеспечения градостроительной деятельности",
-                                "Городская аналитика",
-                                "Другие наши проекты "
+        "GeoMeta",
+        "Государственная система обеспечения градостроительной деятельности",
+        "Городская аналитика",
+        "Другие наши продукты"
         };
+        ArrayList valuesH1H2 = new ArrayList();
+        ArrayList valuesH1H2Result = new ArrayList();
 
-        string Test1()
+
+        ArrayList Test1()
         {
-            string res;
+
+            string[] res = { };
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(urlGems);
             webElement = driver.FindElement(By.LinkText("Продукты"));
 
             webElement.Click();
 
-            res = driver.FindElement(By.CssSelector("h2")).Text;
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            for (int i = 0; i < 4; i++)
+            {
+                var newScrollHeight = js.ExecuteScript(
+                    "window.scrollBy(0,600)"
+                    );
+                Thread.Sleep(1000);
+            }
 
-            //List<IWebElement> webH2 = driver.FindElements(By.CssSelector("body > section.my-150 > div > div > div.col-12.col-md-6.fadeInDown > h1")).ToList();
-            //for (int i = 0; i < webH2.Count; i++)
-            //{
-            //    res = webH2[i].Text;  
-            //}
+            List<IWebElement> webH2 = driver.FindElements(By.CssSelector("h1,h2")).ToList();
+            ArrayList valuesH1H2New = new ArrayList();
+
+            for (int i = 0; i < webH2.Count; i++)
+            {
+                valuesH1H2New.Add(webH2[i].Text);
+            }
 
             driver.Quit();
-            return res;
+            return valuesH1H2New;
         }
 
         [Fact]
         public void passingTest()
         {
+            valuesH1H2.AddRange(values);
+            valuesH1H2Result.AddRange(Test1());
 
-         Test1();
-           
-         Assert.Equal(values[0], Test1());
-            
+            for (int i = 0; i < valuesH1H2.Count; i++)
+            {
+                Assert.Equal(valuesH1H2[i], valuesH1H2Result[i]);
+            }
         }
 
         //[Fact]
         //public void faiillingTest()
         //{
-            
+
         //}
 
     }
